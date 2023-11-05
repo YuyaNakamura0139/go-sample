4b8aa9d437cb15e2ed6c1d3fdda2acfb633217fd
YuyaNakamura0139 <mtk371@icloud.com>
2023-11-05 17:05:42 +0000

    #4 チュートリアルを進める
    - 以下を進めた。
    https://go-tour-jp.appspot.com/basics/1
    - 次回はループ処理から

--- tutorial/basics/main.go
+++ tutorial/basics/main.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
+const (
+	Big   = 1<<100 + 1
+	Small = Big >> 99
+)
+
+func needInt(x int) int { return x*10 + 1 }
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
+
 func main() {
-	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
 }
