80695129b2c53d86689f35509e4b5fdb8e8ca022
YuyaNakamura0139 <mtk371@icloud.com>
2023-11-06 00:33:52 +0000

    #4 チュートリアルを進める
    - 以下まで進めた。
    https://go-tour-jp.appspot.com/flowcontrol/6

--- tutorial/basics/main.go
+++ tutorial/basics/main.go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-const (
-	Big   = 1<<100 + 1
-	Small = Big >> 99
-)
-
-func needInt(x int) int { return x*10 + 1 }
-func needFloat(x float64) float64 {
-	return x * 0.1
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
 }
 
 func main() {
-	fmt.Println(needInt(Small))
-	fmt.Println(needFloat(Small))
-	fmt.Println(needFloat(Big))
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
 }
